2f416b913d1f0103fd99b42c8b1fed2bb7aa9aae
Viktor Petersson <vpetersson@screenly.io>
2020-11-10 18:40:25 +0000

    removes hybrid build.

--- build.sh
+++ build.sh
@@ -2,14 +2,11 @@
 
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 # -*- sh-basic-offset: 4 -*-
+set -euo pipefail
 
-# Only build on ARMv7
-if [ "$(arch)" = "armv7l" ]; then
+cd /src
+/usr/lib/arm-linux-gnueabihf/qt5/bin/qmake
+make -j "$(nproc --all)"
+make install
 
-    cd /src
-    /usr/lib/arm-linux-gnueabihf/qt5/bin/qmake
-    make -j "$(nproc --all)"
-    make install
-
-    tar -zcvf "/build/screenly-webview.tar.gz" res ScreenlyWebview
-fi
+tar -zcvf "/build/screenly-webview.tar.gz" res ScreenlyWebview
